a207e2e42fce3d65663594c43b905f4f06b73f10
Vishnu Mohan <mohanvishnu4699@gmail.com>
2021-10-08 12:14:01 +0530

    Adding the Ajax calls
    this is using traditional method

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/india');
+request.send();
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `<article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${data.population}</p>
+      <p class="country__row"><span>🗣️</span></p>
+      <p class="country__row"><span>💰</span>CUR</p>
+    </div>
+  </article>`;
+});
