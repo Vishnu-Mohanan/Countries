1fc2e445ff41e74a3e08f7b99aad248677463ba0
Vishnu Mohan <mohanvishnu4699@gmail.com>
2021-10-08 12:54:16 +0530

    Almost
    Working for all countries

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/india');
+request.open('GET', 'https://restcountries.com/v3.1/name/russia');
 request.send();
 
 request.addEventListener('load', function () {
@@ -14,13 +14,21 @@ request.addEventListener('load', function () {
   console.log(data);
 
   const html = `<article class="country">
-    <img class="country__img" src="${data.flag}" />
+    <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
+      <h3 class="country__name">${data.name.common}</h3>
       <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${data.population}</p>
-      <p class="country__row"><span>🗣️</span></p>
-      <p class="country__row"><span>💰</span>CUR</p>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} M </p>
+      <p class="country__row"><span>🗣️</span>${Object.values(
+        data.languages
+      )}</p>
+      <p class="country__row"><span>💰</span>${
+        Object.keys(data.currencies)[0]
+      }</p>
     </div>
   </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
